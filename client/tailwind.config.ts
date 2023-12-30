3f53121c0106fb032cc7df8ec5350feca68057c1
Ümitcan <78308730+umtcnzn@users.noreply.github.com>
2023-12-30 20:04:28 +0300

    add necessary files

---
+++ client/tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/primereact/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
+export default config
